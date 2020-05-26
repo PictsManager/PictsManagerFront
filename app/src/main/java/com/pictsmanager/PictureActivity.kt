7f9421925a0a02a6b4ca2c43cc5fe29a91633c00
jeanEpitech <jean.bosc@epitech.eu>
2020-05-26 19:11:40 +0200

    picture rotation fixed

--- app/src/main/java/com/pictsmanager/PictureActivity.kt
+++ app/src/main/java/com/pictsmanager/PictureActivity.kt
@@ -3,22 +3,38 @@ package com.pictsmanager
 import android.content.Intent
 import android.graphics.Bitmap
 import android.graphics.BitmapFactory
+import android.media.ExifInterface
 import android.os.Bundle
 import android.os.PersistableBundle
 import android.util.Log
+import android.util.SparseIntArray
+import android.view.Surface
 import android.view.View
 import android.widget.ImageView
 import androidx.appcompat.app.AppCompatActivity
 import kotlinx.android.synthetic.main.activity_picture.*
 import java.io.File
+import java.io.IOException
 
 class PictureActivity : AppCompatActivity() {
     var imagePictureView: ImageView? = null
+
+    companion object {
+        private val ORIENTATION = SparseIntArray()
+
+        init {
+            ORIENTATION.append(Surface.ROTATION_0, 90)
+            ORIENTATION.append(Surface.ROTATION_90, 0)
+            ORIENTATION.append(Surface.ROTATION_180, 270)
+            ORIENTATION.append(Surface.ROTATION_270, 180)
+        }
+    }
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_picture)
         imagePictureView = findViewById<View>(R.id.imagePictureView) as ImageView
+
 
         val filePath = intent.getStringExtra("PictureTaked")
         var file : File = File(filePath)
@@ -26,6 +42,7 @@ class PictureActivity : AppCompatActivity() {
 
         var bitmap : Bitmap = BitmapFactory.decodeFile(file.absolutePath)
         imagePictureView!!.setImageBitmap(bitmap)
+
         /*if (bundle != null) {
             val resId = bundle.getInt("PictureTaked")
             imagePictureView?.setImageResource(resId)
