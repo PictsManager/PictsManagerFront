f1ac6ba7fde0b1544437a11dfc08d107b7cc9943
jeanEpitech <jean.bosc@epitech.eu>
2020-05-26 14:01:23 +0200

    instantiate activity & layout for viewing picture

---
+++ app/src/main/java/com/pictsmanager/PictureActivity.kt
@@ -0,0 +1,13 @@
+package com.pictsmanager
+
+import android.os.Bundle
+import android.os.PersistableBundle
+import androidx.appcompat.app.AppCompatActivity
+
+class PictureActivity : AppCompatActivity() {
+
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_picture)
+    }
+}
