92b48ad6db6bcd0fd02f2769b8ab9c0b2835a6f7
Arnaud <44978600+Gerroll@users.noreply.github.com>
2020-06-02 18:05:06 +0200

    connexion + creation account => link back (#5)
    * connexion + creation account => link back
    
    * JWT header fetch on connection

--- app/src/main/java/com/pictsmanager/request/api/UsersApi.kt
+++ app/src/main/java/com/pictsmanager/request/api/UsersApi.kt
@@ -1,20 +1,23 @@
 package com.pictsmanager.request.api
 
+import com.pictsmanager.request.model.SuccessModel
 import com.pictsmanager.request.model.UserModel
-/*import retrofit2.Call
+import retrofit2.Call
 import retrofit2.http.Body
 import retrofit2.http.GET
-import retrofit2.http.POST*/
+import retrofit2.http.POST
+import retrofit2.http.Query
 
 
 interface UsersApi {
 
-/*    @GET("/api/users")
-    fun getUserList(): Call<List<UserModel>>*/
+    @GET("/user/login")
+    fun tryConnexion(@Query("email") email: String, @Query("password") password: String): Call<SuccessModel>
+
+    @POST("/user/register")
+    fun tryCreateAccount(@Body userModel: UserModel): Call<UserModel>
 
 /*    @POST("/api/users")
     fun createUser(@Body user: UserModel): Call<UserModel>*/
 
-/*    @POST("/users/login")
-    fun tryConnexion(@Body user: UserModel): Call<UserModel>*/
 }
---
+++ app/src/main/java/com/pictsmanager/request/model/SuccessModel.kt
@@ -0,0 +1,9 @@
+package com.pictsmanager.request.model
+
+class SuccessModel {
+    var message: String? = null
+
+    override fun toString(): String {
+        return "message: $message"
+    }
+}
