b1e055c95a7c8f472074ae28d28b25f2b7edb0d8
jeanEpitech <jean.bosc@epitech.eu>
2020-07-06 09:36:49 +0200

    init seach bar view

--- app/src/main/java/com/pictsmanager/SearchActivity.kt
+++ app/src/main/java/com/pictsmanager/SearchActivity.kt
@@ -1,12 +1,43 @@
 package com.pictsmanager
 
+import android.content.Intent
 import android.os.Bundle
+import android.view.View
+import android.widget.AdapterView
+import android.widget.ArrayAdapter
+import android.widget.Toast
 import androidx.appcompat.app.AppCompatActivity
+import kotlinx.android.synthetic.main.activity_picture.*
+import kotlinx.android.synthetic.main.activity_search.*
 
 class SearchActivity : AppCompatActivity() {
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_search)
+
+        spDateOrder.onItemSelectedListener = object : AdapterView.OnItemSelectedListener {
+            override fun onNothingSelected(p0: AdapterView<*>?) {}
+
+            override fun onItemSelected(
+                adapterView: AdapterView<*>?,
+                view: View?,
+                position: Int,
+                id: Long
+            ) {
+                Toast.makeText(this@SearchActivity,
+                    "You selected ${adapterView?.getItemAtPosition(position).toString()}",
+                    Toast.LENGTH_LONG).show()
+            }
+
+        }
+        initButtons()
+    }
+
+    private fun initButtons() {
+        buttonBackCamera2.setOnClickListener {
+            val intent = Intent(this@SearchActivity, HomeActivity::class.java)
+            startActivity(intent)
+        }
     }
 }
