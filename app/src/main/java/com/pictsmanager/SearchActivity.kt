23ed532451da1f38df48323d72b15e93c3c53ebe
jeanEpitech <jean.bosc@epitech.eu>
2020-07-06 15:54:02 +0200

    init sear bar and order

--- app/src/main/java/com/pictsmanager/SearchActivity.kt
+++ app/src/main/java/com/pictsmanager/SearchActivity.kt
@@ -1,20 +1,27 @@
 package com.pictsmanager
 
+import android.app.SearchManager
+import android.content.Context
 import android.content.Intent
+import android.os.Build
 import android.os.Bundle
+import android.util.Log
+import android.view.Menu
 import android.view.View
-import android.widget.AdapterView
-import android.widget.ArrayAdapter
-import android.widget.Toast
+import android.widget.*
+import androidx.annotation.RequiresApi
 import androidx.appcompat.app.AppCompatActivity
 import kotlinx.android.synthetic.main.activity_picture.*
 import kotlinx.android.synthetic.main.activity_search.*
 
 class SearchActivity : AppCompatActivity() {
 
+
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_search)
+
+        var searchValue = findViewById(R.id.searchInput) as EditText
 
         spDateOrder.onItemSelectedListener = object : AdapterView.OnItemSelectedListener {
             override fun onNothingSelected(p0: AdapterView<*>?) {}
@@ -38,6 +45,24 @@ class SearchActivity : AppCompatActivity() {
         buttonBackCamera2.setOnClickListener {
             val intent = Intent(this@SearchActivity, HomeActivity::class.java)
             startActivity(intent)
+        }
+
+        searchSubmit.setOnClickListener {
+            var date : String
+            if (searchInput.text != null) {
+                if (spDateOrder.selectedItemPosition == 0) {
+                    date = "from the oldest"
+                } else {
+                    date = "from the most recent"
+                }
+                Toast.makeText(this,
+                    "You selected ${searchInput.text} ${date}",
+                    Toast.LENGTH_SHORT).show()
+            } else {
+                Toast.makeText(this,
+                    "No search",
+                    Toast.LENGTH_SHORT).show()
+            }
         }
     }
 }
