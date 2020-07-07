ab18991403c5491fb62615f5e8c0a9a0c28c4c99
jeanEpitech <jean.bosc@epitech.eu>
2020-07-07 14:47:53 +0200

    button zoom for album fragment correction

--- app/src/main/java/com/pictsmanager/ui/main/AlbumFragment.kt
+++ app/src/main/java/com/pictsmanager/ui/main/AlbumFragment.kt
@@ -91,7 +91,7 @@ class AlbumFragment(context: Context): Fragment() {
                     true
                 }
                 R.id.action_zoom -> {
-                    false
+                    true
                 }
                 else -> false
             }
