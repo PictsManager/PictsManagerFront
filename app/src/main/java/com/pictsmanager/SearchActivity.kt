fbf21be8be53c510f2ac238100e4637a660acf07
jeanEpitech <jean.bosc@epitech.eu>
2020-07-07 11:25:01 +0200

    gallery of images searched v1

--- app/src/main/java/com/pictsmanager/SearchActivity.kt
+++ app/src/main/java/com/pictsmanager/SearchActivity.kt
@@ -3,9 +3,11 @@ package com.pictsmanager
 import android.app.SearchManager
 import android.content.Context
 import android.content.Intent
+import android.graphics.Bitmap
 import android.os.Build
 import android.os.Bundle
 import android.util.Log
+import android.view.LayoutInflater
 import android.view.Menu
 import android.view.View
 import android.widget.*
@@ -13,7 +15,12 @@ import androidx.annotation.RequiresApi
 import androidx.appcompat.app.AppCompatActivity
 import com.pictsmanager.request.model.ImageModel
 import com.pictsmanager.request.service.GlobalService
+import com.pictsmanager.ui.main.ImageFragment
 import com.pictsmanager.util.GlobalStatus
+import com.pictsmanager.util.Huffman
+import com.pictsmanager.util.ImageGalleryAdapter
+import com.pictsmanager.util.RLE
+import kotlinx.android.synthetic.main.activity_connexion.*
 import kotlinx.android.synthetic.main.activity_picture.*
 import kotlinx.android.synthetic.main.activity_search.*
 import org.json.JSONObject
@@ -22,13 +29,17 @@ import retrofit2.Callback
 import retrofit2.Response
 
 class SearchActivity : AppCompatActivity() {
-
+    var images: ArrayList<ImageModel> = ArrayList()
+    lateinit var gridView: GridView
+    lateinit var imageAdapter: ImageGalleryAdapter
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_search)
 
         var searchValue = findViewById(R.id.searchInput) as EditText
+        gridView =findViewById(R.id.searchGridView) as GridView
+
 
         spDateOrder.onItemSelectedListener = object : AdapterView.OnItemSelectedListener {
             override fun onNothingSelected(p0: AdapterView<*>?) {}
@@ -45,6 +56,7 @@ class SearchActivity : AppCompatActivity() {
             }
 
         }
+
         initButtons()
     }
 
@@ -57,14 +69,12 @@ class SearchActivity : AppCompatActivity() {
         searchSubmit.setOnClickListener {
             var order : Boolean
             if (searchInput.text != null) {
-                if (spDateOrder.selectedItemPosition == 0) {
-                    // From the oldest"
-                    order = true
-                } else {
-                    // From the most recent
-                    order = false
-                }
-                requestForSearching(searchInput.text.toString(), order)
+                // From the oldest
+                order = spDateOrder.selectedItemPosition == 0
+
+
+                requestForSearching(adaptTagsString(searchInput.text.toString()), order)
+
                 Toast.makeText(this,
                     "You selected ${searchInput.text} ${order}",
                     Toast.LENGTH_SHORT).show()
@@ -76,9 +86,15 @@ class SearchActivity : AppCompatActivity() {
         }
     }
 
+    private fun adaptTagsString(tags: String) : String {
+        var result =  tags.replace(", ", ",", ignoreCase = false)
+        return result.replace(" ,", ",", ignoreCase = false)
+    }
+
     private fun requestForSearching(tags: String, order: Boolean) {
         val imageReadRequest = GlobalService.imageService.searchImage(GlobalStatus.JWT, tags, true, order)
-        /*imageReadRequest.enqueue(object : Callback<ArrayList<ImageModel>> {
+        Log.d("TAGS ", tags)
+        imageReadRequest.enqueue(object : Callback<ArrayList<ImageModel>> {
             override fun onResponse(
                 call: Call<ArrayList<ImageModel>>,
                 response: Response<ArrayList<ImageModel>>
@@ -87,25 +103,48 @@ class SearchActivity : AppCompatActivity() {
                     val jsonObject = JSONObject(response.errorBody()!!.string())
 
                     System.out.println(jsonObject)
-                    Toast.makeText(ctx, jsonObject.toString(), Toast.LENGTH_SHORT).show()
+                    //Toast.makeText(ctx, jsonObject.toString(), Toast.LENGTH_SHORT).show()
                 } else if (response.code() == 200) {
                     val body = response.body()
+                    Log.d("SUCCESS", response.body().toString())
                     body?.let {
                         images = it
                         completeImageModelWithDecompressBitmap(images)
                         resetGridViewAndImageSelected()
-                        Toast.makeText(ctx, "Successful update", Toast.LENGTH_SHORT).show()
+                        //Toast.makeText(ctx, "Successful update", Toast.LENGTH_SHORT).show()
                     }
                 } else {
                     System.out.println("Untreated error")
-                    Toast.makeText(ctx, "Untreated error", Toast.LENGTH_SHORT).show()
+                    //Toast.makeText(ctx, "Untreated error", Toast.LENGTH_SHORT).show()
                 }
             }
 
             override fun onFailure(call: Call<ArrayList<ImageModel>>, t: Throwable) {
                 Log.d("ERR", t.toString())
-                Toast.makeText(ctx, "ERROR server: read", Toast.LENGTH_LONG).show()
+                //Toast.makeText(ctx, "ERROR server: read", Toast.LENGTH_LONG).show()
             }
-        })*/
+        })
+    }
+
+    private fun completeImageModelWithDecompressBitmap(toComplete: ArrayList<ImageModel>) {
+        if (GlobalStatus.COMPRESSION == "RLE") {
+            for (im in toComplete) {
+                val bmp: Bitmap = RLE.applyDecompress(im.image, im.width, im.height)
+                im.imageBM = bmp
+            }
+        } else {
+            for (im in toComplete) {
+                val keys = arrayOf<Array<String>>(im.red, im.green, im.blue)
+                val bmp: Bitmap = Huffman.applyDecompress(im.image, im.width, im.height, keys)
+                im.imageBM = bmp
+            }
+        }
+    }
+
+    private fun resetGridViewAndImageSelected() {
+        imageAdapter = ImageGalleryAdapter(this, images)
+        gridView.adapter = imageAdapter
+
+        imageAdapter.notifyDataSetChanged()
     }
 }
